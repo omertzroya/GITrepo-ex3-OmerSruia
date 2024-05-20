c8f371451f3248177542cb00ad257167f01c5ed3
Omer <omersr9@gmail.com>
2024-05-20 11:28:26 +0300

    after the change

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 public class Hello {
     public static void main(String[] args) {
         for(int i=0; i<10; i++) {
-            System.out.println("Hello:"+i);
+            System.out.println("Hello:My EX2"+i);
         }
     }
 }
