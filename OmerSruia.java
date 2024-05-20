ababb415908118d4634d448654e081f656e76fc6
Omer <omersr9@gmail.com>
2024-05-20 10:57:44 +0300

    upload files to git

---
+++ OmerSruia.java
@@ -0,0 +1,9 @@
+public class OmerSruia {
+
+    public static void main(String[] args) {
+       // print name several times
+         for (int i = 0; i < 10; i++) {
+              System.out.println("Omer Sruia");
+         }
+    }
+}
